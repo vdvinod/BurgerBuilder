366478d63c5aedbf23ed84e2d2e67da276f942da
vinod singh rawat <vdvinodrawat@gmail.com>
2020-01-12 09:55:33 +0530

    added project configuration and created folder structure

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import Classes from './Layout.css';
+const layout = (props) => (
+    <Aux>
+        <div></div>
+        <main className={Classes.content}>
+
+        </main>
+
+    </Aux>
+)
+
+export default layout;
+
