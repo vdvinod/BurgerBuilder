d242884ee9ef466dff07050d1a9972b86c69fd6b
vinod singh rawat <vdvinodrawat@gmail.com>
2020-02-02 23:31:14 +0530

    replaced px with rem

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://react-my-project-d9d88.firebaseio.com/"
+    baseURL: 'https://react-my-burger.firebaseio.com/'
 });
 
 export default instance;
